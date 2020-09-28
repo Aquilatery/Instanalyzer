09ce4dddee253ac57d55026e366028fa5638ade8
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-09-29 00:19:40 +0300

    53X Again 2!

---
+++ Instanalyzer/Instanalyzer.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Forms;
+
+namespace Instanalyzer
+{
+    static class Instanalyzer
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
